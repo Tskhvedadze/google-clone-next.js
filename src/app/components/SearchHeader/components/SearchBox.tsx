d386b754a80eb0380ab838fd2f3aa9dc370d1cb2
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-14 14:47:07 +0400

    complete the search box component

--- src/app/components/SearchHeader/components/SearchBox.tsx
+++ src/app/components/SearchHeader/components/SearchBox.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import {
+  ReadonlyURLSearchParams,
+  useSearchParams,
+  useRouter,
+} from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+import { RxCross2 } from "react-icons/rx";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+
 export const SearchBox = () => {
-  return <div>SearchBox</div>;
+  const searchParams: ReadonlyURLSearchParams = useSearchParams();
+  const router: AppRouterInstance = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+    >
+      <input
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setTerm(e.target.value)}
+        value={term}
+        type="text"
+        className="w-full focus:outline-none "
+      />
+      <RxCross2
+        onClick={() => setTerm("")}
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+      />
+      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+      <AiOutlineSearch
+        onClick={handleSubmit}
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+      />
+    </form>
+  );
 };
--- src/app/components/SearchHeader/components/SearchHeaderOptions.tsx
+++ src/app/components/SearchHeader/components/SearchHeaderOptions.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { AiOutlineSearch } from "react-icons/ai";
+import {
+  ReadonlyURLSearchParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+
 export const SearchHeaderOptions = () => {
-  return <div>SearchHeaderOptions</div>;
+  const pathName = usePathname();
+  const router: AppRouterInstance = useRouter();
+  const searchParams: ReadonlyURLSearchParams = useSearchParams();
+
+  const searchTerm = searchParams.get("term");
+
+  function selectTab(tab: string) {
+    router.push(
+      `/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`
+    );
+  }
+
+  return (
+    <div>
+      <div
+        // onClick={() => selectTab("Images")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathName === "/search/web" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
+        <AiOutlineSearch className="text-md" />
+        <p>All</p>
+      </div>
+    </div>
+  );
 };
