052d4452620848b61348eb9ba4c0fd9dfc762244
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-14 15:35:24 +0400

    complete the search header options

--- src/app/components/SearchHeader/components/SearchHeaderOptions.tsx
+++ src/app/components/SearchHeader/components/SearchHeaderOptions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineCamera } from "react-icons/ai";
 import {
   ReadonlyURLSearchParams,
   usePathname,
@@ -14,24 +14,34 @@ export const SearchHeaderOptions = () => {
   const router: AppRouterInstance = useRouter();
   const searchParams: ReadonlyURLSearchParams = useSearchParams();
 
-  const searchTerm = searchParams.get("term");
+  const searchTerm = searchParams.get("searchTerm");
 
-  function selectTab(tab: string) {
+  function selectTab(tab: "web" | "image") {
     router.push(
-      `/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`
+      `/search/${tab === "image" ? "image" : "web"}?searchTerm=${searchTerm}`
     );
   }
 
   return (
-    <div>
+    <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
       <div
-        // onClick={() => selectTab("Images")}
+        onClick={() => selectTab("web")}
         className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
           pathName === "/search/web" && "!text-blue-600 !border-blue-600"
         }`}
       >
         <AiOutlineSearch className="text-md" />
         <p>All</p>
+      </div>
+
+      <div
+        onClick={() => selectTab("image")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
+          pathName === "/search/image" && "!text-blue-600 !border-blue-600"
+        }`}
+      >
+        <AiOutlineCamera className="text-md" />
+        <p>Images</p>
       </div>
     </div>
   );
