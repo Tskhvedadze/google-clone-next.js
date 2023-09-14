7c70a8cc9b120b74e8f98b741aaaca16d352bc78
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-14 16:04:51 +0400

    fetch data from google search api && show the titles

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,22 @@
-function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+async function WebSearchPage({
+  searchParams: { searchTerm },
+}: {
+  searchParams: { searchTerm: string };
+}) {
+  const response = await fetch(
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}`
+  );
+  const data = await response.json();
+  const results = data?.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result: any) => (
+          <h1 key={result.title}>{result.title}</h1>
+        ))}
+    </>
+  );
 }
 
 export default WebSearchPage;
